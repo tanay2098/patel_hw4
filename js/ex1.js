fb452efcad5e8f6f864e7e30a5b1ba693868f8b1
Tanay Patel <tanay.patel@student.csulb.edu>
2021-02-13 11:46:13 -0800

    ex1

---
+++ js/ex1.js
@@ -0,0 +1,31 @@
+let num1=Number(prompt('Enter the first number: '));
+let num2=Number(prompt('Enter the second number: '));
+let operator=prompt('Enter anyone operator from (+,-,*,/):');
+
+function calculate(num1,num2){
+    switch(operator){
+        case '+':
+            console.log(`${num1},${num2},${operator}`);
+            let add=num1+num2;
+            console.log(`${num1} + ${num2}=${add}`);
+            break;
+
+        case '-':
+            console.log(`${num1},${num2},${operator}`);
+            let subtract=num1+num2;
+            console.log(`${num1} - ${num2}=${subtract}`);
+            break;
+        case '*':
+            console.log(`${num1},${num2},${operator}`);
+            let product=num1*num2;
+            console.log(`${num1} x ${num2}=${product}`);
+            break;
+        case '/':
+            console.log(`${num1},${num2},${operator}`);
+            let divide=num1/num2;
+            console.log(`${num1} / ${num2}=${divide}`);
+            break;
+    }
+}
+
+calculate(num1,num2);
