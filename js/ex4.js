928784f54b837977b888d8bdd6b6721d90e89be3
Tanay Patel <tanay.patel@student.csulb.edu>
2021-02-15 20:28:41 -0800

    ex4_commit

---
+++ js/ex4.js
@@ -0,0 +1,16 @@
+const word_list=[];
+let word_prompt=prompt('Enter any word: ');
+
+if(word_prompt.toLowerCase()=='stop'){
+    console.log('No more words to display, Stop was the first word entered.');
+}
+
+
+while(word_prompt.toLowerCase()!='stop'){
+    word_list.push(word_prompt);
+    word_prompt=prompt('Enter any word: ');
+}
+
+for (let i=0; i<word_list.length;i++){
+    console.log(word_list[i]);
+}
